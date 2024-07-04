4fa9a797674b0c7874c161a34232604fa2a2cde7
maximilianbauer <maximilian_.bauer@icloud.com>
2024-07-04 10:25:33 +0200

    design changes

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,16 +1,28 @@
+
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.launch
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import util.Gelds
 import util.toHumanReadableString
@@ -28,12 +40,6 @@ fun App() {
 @Preview
 fun Screen() {
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("Rags To Riches") },
-                backgroundColor = MaterialTheme.colors.primary
-            )
-        },
         content = {
             val coroutineScope = rememberCoroutineScope()
             val viewModel by remember {
@@ -55,35 +61,68 @@ fun Screen() {
             }
 
             Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
+                modifier = Modifier.fillMaxWidth()
                     .verticalScroll(rememberScrollState())
+                    .padding(
+                        horizontal = 100.dp,
+                    ),
+                horizontalAlignment = Alignment.End
             ) {
-                GreetingSection()
+                Button(
+                    onClick = {}
+                ) {
+
+                    Text("Cash " + currentMoney?.toHumanReadableString())
+                }
+            }
+
+
+
+            Column(
+                modifier = Modifier.fillMaxWidth()
+                    .verticalScroll(rememberScrollState())
+
+            ) {
+
+
+
+                Text(
+                    "Rags To Riches",
+                    style = MaterialTheme.typography.h1,
+                )
                 Spacer(modifier = Modifier.height(16.dp))
 
                 Button(
+                    modifier = Modifier.padding(18.dp),
                     onClick = { viewModel.reset() },
-                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Yellow)
-                ) {
+                  colors = ButtonDefaults.buttonColors(
+                      backgroundColor = Color(255,165,0),
+                      contentColor = Color.White
+                  ),
+                )
+                {
                     Text("Reset Game")
                 }
 
                 gameState?.let { state ->
-                    Spacer(modifier = Modifier.height(16.dp))
                     Text(
                         "Bank: ${currentMoney?.toHumanReadableString()} Gelds",
-                        style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.Bold),
-                        color = MaterialTheme.colors.primary
+                        style = MaterialTheme.typography.h4,
                     )
-                    Spacer(modifier = Modifier.height(16.dp))
-
                     Button(
-                        onClick = { viewModel.clickMoney(state) },
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green)
+                        modifier = Modifier.padding(18.dp),
+                        onClick = { viewModel.clickMoney(state) }
                     ) {
-                        Text("Click to Earn Money")
+                        Text("Click To Sell Lemonade")
+                    }
+
+                    state.availableJobs.forEach { availableJob ->
+                        Generator(
+                            gameJob = availableJob,
+                            alreadyBought = state.workers.any { it.jobId == availableJob.id },
+                            onBuy = { viewModel.addWorker(state, availableJob) },
+                            onUpgrade = { viewModel.upgradeJob(state, availableJob) }
+                        )
                     }
                 }
             }
@@ -92,30 +131,37 @@ fun Screen() {
 }
 
 @Composable
-fun GreetingSection() {
-    Card(
-        shape = RoundedCornerShape(8.dp),
-        backgroundColor = MaterialTheme.colors.surface,
-        modifier = Modifier.fillMaxWidth()
+private fun Generator(
+    gameJob: GameJob,
+    alreadyBought: Boolean,
+    modifier: Modifier = Modifier,
+    onBuy: () -> Unit = {},
+    onUpgrade: () -> Unit = {},
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier
+            .padding(8.dp)
+            .background(Color.Yellow, RoundedCornerShape(8.dp))
+            .padding(8.dp)
     ) {
-        Column(
-            modifier = Modifier.padding(16.dp)
-        ) {
-            Text(
-                text = "Welcome to Rags To Riches!",
-                style = MaterialTheme.typography.h5.copy(
-                    fontWeight = FontWeight.Bold,
-                    color = MaterialTheme.colors.onSurface
-                )
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = "Start your journey by clicking the button below to earn money.",
-                style = MaterialTheme.typography.body1.copy(
-                    color = MaterialTheme.colors.onSurface
-                )
-            )
+        Column {
+            Text("Classic L. Machine ${gameJob.id}")
+            Text("Level: ${gameJob.level.level}")
+            Text("Costs: ${gameJob.level.cost.toHumanReadableString()} Money")
+            Text("Earns: ${gameJob.level.earn.toHumanReadableString()} Money")
+            Text("Duration: ${gameJob.level.duration.inWholeSeconds} Seconds")
+        }
+        if (!alreadyBought) {
+            Button(onClick = onBuy) {
+                Text("Buy")
+
+            }
+        } else {
+            Text("Bought")
+        }
+        Button(onClick = onUpgrade) {
+            Text("Upgrade")
         }
     }
 }
-
--- composeApp/src/commonMain/kotlin/util/ToHumanReadableString.kt
+++ composeApp/src/commonMain/kotlin/util/ToHumanReadableString.kt
@@ -1,10 +1,16 @@
 package util
 
+import com.ionspin.kotlin.bignum.integer.toBigInteger
+
 fun Gelds.toHumanReadableString(): String = when (exponent) {
     in 0..2 -> "${(significand)}"
     in 3..5 -> "${(significand / 1000)},${significand % 1000}k"
     in 6..8 -> "${(significand / 1000000)},${significand % 1000}M"
     in 9..11 -> "${(significand / 1000000000)},${significand % 1000}B"
     in 12..14 -> "${(significand / 1000000000000)},${significand % 1000}T"
-    else -> "A lot"
-}
+    in 15..17 -> "${(significand / 1000000000000000)},${significand % 1000}Q"
+    in 15..17 -> "${(significand / 1000000000000000000)},${significand % 1000}Qa"
+    in 18..20 -> "${(significand / 10.toBigInteger().pow(18))},${significand % 1000}Qt"
+    in 21..23 -> "${(significand / 10.toBigInteger().pow(21))},${significand % 1000}Qa"
+
+    else -> "You Are Rich" }
